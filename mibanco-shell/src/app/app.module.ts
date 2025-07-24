bb4fb3aa9fa849d4720d738285eeebd82f19b65a
haroldp.delfosti <harold.portillo@delfosti.com>
2025-07-24 13:47:24 -0500

    agregar iconos

--- mibanco-shell/src/app/app.module.ts
+++ mibanco-shell/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -12,6 +12,7 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
